28b817011b81288f2b1d1f31d2c1bc2d9c82df50
Habiba618 <82892084+Habiba618@users.noreply.github.com>
2023-12-13 11:18:32 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Name: ");
+        String ad = sc.nextLine();
+        System.out.print("Password: ");
+        String sifre = sc.nextLine();
+        if (ad.equals("Admin") && sifre.equals("12345")){
+            System.out.println("Daxil oldunuz...");
+        } else if ((ad.equals("Admin") && !sifre.equals("12345"))) {
+            System.out.println("Sifre yanlisdir..");
+            System.out.println("1 -> sifre oldugu kimi qalsin");
+            System.out.println("2 -> sifre deyisilsin");
+            int value = sc.nextInt();
+            sc.nextLine();
+            switch (value){
+                case 1:
+                    System.out.println("Yeniden cehd edin");
+                    break;
+                case 2:
+
+            System.out.print("New Password: ");
+            String yeniSifre = sc.nextLine();
+            if (yeniSifre.equals("12345")){
+                System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+            }else{
+                System.out.println("Şifre oluşturuldu");
+        }break;  }}
+            else{
+            System.out.println("istifadeci adi ve ya sifre dogru deyil...");
+        }
     }
 }
